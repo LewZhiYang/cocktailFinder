b06a0369b1d149aba07a658ac7a3cd65be23d462
LewZhiYang <circle1329@gmail.com>
2022-01-26 01:13:32 +0800

    throw in work from before

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+document.querySelector("button").addEventListener("click", getCocktail);
+
+function getCocktail (){
+    input = document.querySelector("input").value;
+    url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + input
+    fetch(url)
+    .then(response => response.json())
+    .then(data => (data.drinks[0]))
+    .then(data =>
+        
+        (document.querySelector("img").src = data.strDrinkThumb,
+        document.querySelector("h3").textContent = data.strInstructions,
+        document.querySelector("h2").textContent = data.strDrink));
+}
